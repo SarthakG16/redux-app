60344811d1d29e9e8b5edf2884697cafb000785c
SarthCoder <sarthakganoorkar@gmail.com>
2022-07-28 22:18:58 +0800

    Currying Functions

--- src/index.js
+++ src/index.js
@@ -3,8 +3,11 @@ import {compose, pipe} from "lodash/fp"
 const trim = str => str.trim();
 const toLowerCase = str => str.toLowerCase();
 const wrapInDiv = str => `<div>${str}</div>`
+const wrapInSpan = str => `<span>${str}</span>`
+const wrap = (type, str) => `<${type}>${str}</${type}>`
+const curryingWrap = (type) => (str) =>`<${type}>${str}</${type}>`
 
-const transform = pipe(trim, toLowerCase, wrapInDiv);
+const transform = pipe(trim, toLowerCase, curryingWrap("span"));
 
 let message = transform("   java   ");
 console.log(message);
